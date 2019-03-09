0017d19e3d420ac77caa47155ee81a54a7be349a
Johnny Huang <jtk155586@gmail.com>
2019-03-10 00:10:40 +0800

    fix bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,16 +12,4 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.dataService.loadShoppingCart();
   }
-
-  onActivate() {
-    // scroll to top
-    const scrollToTop = window.setInterval(() => {
-      const pos = window.pageYOffset;
-      if (pos > 0) {
-        window.scrollTo(0, pos - 20); // how far to scroll on each step
-      } else {
-        window.clearInterval(scrollToTop);
-      }
-    }, 16);
-  }
 }
